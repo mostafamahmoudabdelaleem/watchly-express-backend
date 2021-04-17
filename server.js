ea2fd40d62b6ea1700321477bf0e42eca43b0f62
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2021-04-18 00:08:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 //App Routes
 app.use("/api/movies/", moviesRouter);
 app.use("/api/series/", seriesRouter);
-app.use("/api/ramdan/", ramadanRouter);
+app.use("/api/ramadan/", ramadanRouter);
 app.use("/log/", logRouter);
 
 //Redirect default entry to watchly.tk
